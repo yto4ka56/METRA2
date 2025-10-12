6859165f15d1354460582e5caccfb87f4c3ed922
maxim <maxim.beliaev18@gmail.com>
2025-10-12 20:28:06 +0300

    убраны комментарии

--- GilbMetricCalculator.cs
+++ GilbMetricCalculator.cs
@@ -9,35 +9,28 @@ public class GilbMetricCalculator
     private int maxDepth = 0;
     private int totalLinesOfCode = 0;
     
-    // Swift ключевые слова для метрики Джилба
     private static readonly string[] controlKeywords = {
         "if", "else", "else if", "switch", "case", "for", "while", "repeat"
     };
 
     public void Calculate(string swiftCode)
     {
-        // 1. Инициализация и подготовка данных
         absoluteComplexity = 0;
         maxDepth = 0;
-        // Расчет LOC для S_rel
         totalLinesOfCode = swiftCode.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
 
         string cleanedCode = CleanCode(swiftCode);
         string[] lines = cleanedCode.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-        // 2. Расчет S_abs
-        // Демонстрация использования оператора FOR
+        
         for (int i = 0; i < lines.Length; i++)
         {
             string line = lines[i].Trim();
             
-            // Демонстрация использования оператора WHILE
             int startIndex = 0;
             while (startIndex < line.Length) 
             {
                 bool found = false;
                 
-                // Итерация по ключевым словам
                 foreach (string keyword in controlKeywords) 
                 {
                     if (line.Substring(startIndex).StartsWith(keyword + " "))
@@ -56,7 +49,6 @@ public class GilbMetricCalculator
             }
         }
         
-        // 3. Расчет MaxDepth
         CalculateMaxDepthSimplified(cleanedCode);
     }
     
@@ -67,13 +59,11 @@ public class GilbMetricCalculator
         code = code.Replace("{", " { ").Replace("}", " } ");
         return code;
     }
-
-    // Демонстрация IF-ELSE и SWITCH
+    
     private void CalculateMaxDepthSimplified(string code)
     {
         int currentDepth = 0;
         
-        // Демонстрация оператора DO-WHILE
         int index = 0;
         bool keepLooping;
         
@@ -83,19 +73,14 @@ public class GilbMetricCalculator
             if (index < code.Length) 
             {
                 char c = code[index];
-
-                // IF-ELSE для управления глубиной
+                
                 if (c == '{')
                 {
                     currentDepth++;
-                    // Оператор множественного выбора (SWITCH) для обновления MaxDepth
                     switch (currentDepth)
                     {
                         case int d when d > maxDepth:
                             maxDepth = d;
-                            break;
-                        default:
-                            // Ничего не делаем
                             break;
                     }
                 }
@@ -109,8 +94,7 @@ public class GilbMetricCalculator
             }
         } while (keepLooping);
     }
-
-    // Методы доступа
+    
     public int GetAbsoluteComplexity() => absoluteComplexity;
     
     public double GetRelativeComplexity() 
--- TestSwift.swift
+++ TestSwift.swift
@@ -4,36 +4,32 @@
     func calculateScore(data: [Int]) -> Int {
         var score = 0
         
-        // 1. Условие IF (S_abs +1)
         if data.isEmpty {
             return 0
         }
         
-        // 2. Цикл FOR-IN (S_abs +1)
-        for value in data { // Вложенность: 1
+        for value in data { 
             
-            // 3. Условие IF-ELSE (S_abs +1)
-            if value > 10 { // Вложенность: 2
+  
+            if value > 10 {
                 score += 10
             } else {
                 score += value
             }
             
-            // 4. Цикл WHILE (S_abs +1)
-            var temp = value // Вложенность: 2
+            var temp = value
             while temp > 0 {
                 temp -= 1
                 
-                // 5. Условие IF-ELSE IF-ELSE (S_abs +2)
-                if temp % 3 == 0 { // Вложенность: 3
+                
+                if temp % 3 == 0 { 
                     
-                    // 6. Цикл REPEAT-WHILE (S_abs +1)
-                    repeat { // Вложенность: 4
+                    
+                    repeat {
                         print("Iterating...")
-                    } while temp < 0 // Вложенность: 4
+                    } while temp < 0
                     
-                } else if temp % 2 == 0 { // Вложенность: 3
-                    // Многострочный /* комментарий */
+                } else if temp % 2 == 0 { 
                     /* Это не должно
                        учитываться 
                        в LOC или S_abs */
@@ -41,16 +37,16 @@
                 } else {
                     break
                 }
-            } // Конец WHILE
-        } // Конец FOR-IN
+            } 
+        } 
         
-        // 7. Оператор SWITCH (S_abs +1)
-        switch score { // Вложенность: 1
-            case 0: // S_abs +1
+      
+        switch score { 
+            case 0: 
                 print("Zero score")
-            case 1...10: // S_abs +1
+            case 1...10: 
                 print("Low score")
-            default: // S_abs +1
+            default: 
                 print("High score")
         }
         
